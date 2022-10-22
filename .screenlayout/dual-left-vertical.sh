949f695511b233d6956441d387819034b6be41a6
chenghuipeng <chenghpchn@gmail.com>
2022-10-22 17:24:02 +0800

    dual monitor setting

---
+++ .screenlayout/dual-left-vertical.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-A-0 --mode 3840x2160 --pos 0x0 --rotate left --output DisplayPort-0 --primary --mode 3840x2160 --pos 2160x933 --rotate normal --output DisplayPort-1 --off
