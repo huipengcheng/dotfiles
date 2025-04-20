aa3e44b9d7fdb58645cf027b471c034d2942e69d
huipengcheng <cheng.huipeng@outlook.com>
2025-04-20 18:20:43 +0800

    Make enter key to confirm completion

--- home/.config/nvim/lua/plugins/autocompletion.lua
+++ home/.config/nvim/lua/plugins/autocompletion.lua
@@ -70,6 +70,7 @@ return {
 					--  This will expand snippets if the LSP sent a snippet.
 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
 					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+					["<Enter>"] = cmp.mapping.confirm({ select = true }),
 
 					-- If you prefer more traditional completion keymaps,
 					-- you can uncomment the following lines
