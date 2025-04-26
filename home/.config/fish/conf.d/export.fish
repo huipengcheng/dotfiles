ecb118891a928f50eeb3b79b274c172e012dd4fa
huipengcheng <cheng.huipeng@outlook.com>
2025-04-26 20:40:44 +0800

    Add EDITOR env

--- home/.config/fish/conf.d/export.fish
+++ home/.config/fish/conf.d/export.fish
@@ -1,3 +1,5 @@
+export EDITOR=vim
+
 export MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
 abbr -a --position anywhere -- --help '--help | bat -plhelp'
 abbr -a --position anywhere -- -h '-h | bat -plhelp'
