0766892a0cab7641ae59cc95269ea38a4828d493
huipengcheng <cheng.huipeng@outlook.com>
2025-05-02 19:42:44 +0800

    Enable proxy when on arch & Enable vim mode on fish_vim_mode env var

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -17,20 +17,28 @@ function fish_user_key_bindings
     # The argument specifies the initial mode (insert, "default" or visual).
     fish_vi_key_bindings --no-erase insert
 end
-# Emulates vim's cursor shape behavior
-# Set the normal and visual mode cursors to a block
-set fish_cursor_default block
-# Set the insert mode cursor to a line
-set fish_cursor_insert line
-# Set the replace mode cursors to an underscore
-set fish_cursor_replace_one underscore
-set fish_cursor_replace underscore
-# Set the external cursor to a line. The external cursor appears when a command is started.
-# The cursor shape takes the value of fish_cursor_default when fish_cursor_external is not specified.
-set fish_cursor_external line
-# The following variable can be used to configure cursor shape in
-# visual mode, but due to fish_cursor_default, is redundant here
-set fish_cursor_visual block
+
+if not set -q fish_vim_mode; or test "$fish_vim_mode" = "enable"
+    # Emulates vim's cursor shape behavior
+    # Set the normal and visual mode cursors to a block
+    set fish_cursor_default block
+    # Set the insert mode cursor to a line
+    set fish_cursor_insert line
+    # Set the replace mode cursors to an underscore
+    set fish_cursor_replace_one underscore
+    set fish_cursor_replace underscore
+    # Set the external cursor to a line. The external cursor appears when a command is started.
+    # The cursor shape takes the value of fish_cursor_default when fish_cursor_external is not specified.
+    set fish_cursor_external line
+    # The following variable can be used to configure cursor shape in
+    # visual mode, but due to fish_cursor_default, is redundant here
+    set fish_cursor_visual block
+end
+
+
+if test "$hostname" = "arch"
+    proxy_on > /dev/null
+end
 
 
 # kitty 在 DoomOne 下，suggestion 的颜色太浅了，调深一点
