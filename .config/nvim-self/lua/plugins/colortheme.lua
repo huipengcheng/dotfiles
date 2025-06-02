f3e5aa71b0772005167ef1674e09c5c5123f1bdc
huipengcheng <cheng.huipeng@outlook.com>
2025-06-02 21:39:43 +0800

    Add some nvim-self config

--- .config/nvim-self/lua/plugins/colortheme.lua
+++ .config/nvim-self/lua/plugins/colortheme.lua
@@ -1,106 +1,72 @@
 return {
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+
+	-- tokyonight
+	{
 		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("tokyonight").setup({
-				styles = {
-					comments = { italic = false }, -- Disable italics in comments
-				},
-			})
-
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			-- vim.cmd.colorscheme("tokyonight")
-		end,
+		lazy = true,
+		opts = { style = "moon" },
 	},
 
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = true,
-	},
-	{
-		"NTBBloodbath/doom-one.nvim",
-		-- config = function()
-		-- 	vim.cmd.colorscheme("doom-one")
-		-- end,
-	},
 	{
 		"romgrk/doom-one.vim",
-		config = function()
-			vim.cmd.colorscheme("doom-one")
-		end,
+		lazy = true,
 	},
-	{
-		"rebelot/kanagawa.nvim",
-		config = function()
-			-- vim.cmd.colorscheme("kanagawa")
-		end,
-	},
-	{
-		"thesimonho/kanagawa-paper.nvim",
-		config = function()
-			-- vim.cmd.colorscheme("kanagawa-paper")
-		end,
-	},
-	{
-		"neanias/everforest-nvim",
-	},
-	{
-		"bluz71/vim-moonfly-colors",
-		config = function()
-			-- vim.cmd.colorscheme("moonfly")
-		end,
-	},
-	{
-		"bluz71/vim-nightfly-colors",
-		config = function()
-			-- vim.cmd.colorscheme("nightfly")
-		end,
-	},
+
+	-- catppuccin
 	{
 		"catppuccin/nvim",
-		config = function()
-			-- latte, frappe, macchiato, mocha
-			-- vim.cmd.colorscheme("catppuccin-macchiato")
-		end,
-	},
-	{
-		"EdenEast/nightfox.nvim",
-		config = function()
-			-- nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
-			-- vim.cmd.colorscheme("terafox")
-		end,
-	},
-	{
-		"sainnhe/gruvbox-material",
-		config = function()
-			-- vim.cmd.colorscheme("gruvbox-material")
-		end,
-	},
-	{
-		"fynnfluegge/monet.nvim",
-		config = function()
-			-- vim.cmd.colorscheme("monet")
-		end,
-	},
-	{
-		"pineapplegiant/spaceduck",
-		config = function()
-			-- vim.cmd.colorscheme("spaceduck")
-		end,
-	},
-	{
-		"loctvl842/monokai-pro.nvim",
-		config = function()
-			-- vim.cmd.colorscheme("monokai-pro")
-		end,
+		lazy = true,
+		name = "catppuccin",
+		opts = {
+			integrations = {
+				aerial = true,
+				alpha = true,
+				cmp = true,
+				dashboard = true,
+				flash = true,
+				fzf = true,
+				grug_far = true,
+				gitsigns = true,
+				headlines = true,
+				illuminate = true,
+				indent_blankline = { enabled = true },
+				leap = true,
+				lsp_trouble = true,
+				mason = true,
+				markdown = true,
+				mini = true,
+				native_lsp = {
+					enabled = true,
+					underlines = {
+						errors = { "undercurl" },
+						hints = { "undercurl" },
+						warnings = { "undercurl" },
+						information = { "undercurl" },
+					},
+				},
+				navic = { enabled = true, custom_bg = "lualine" },
+				neotest = true,
+				neotree = true,
+				noice = true,
+				notify = true,
+				semantic_tokens = true,
+				snacks = true,
+				telescope = true,
+				treesitter = true,
+				treesitter_context = true,
+				which_key = true,
+			},
+		},
+		specs = {
+			{
+				"akinsho/bufferline.nvim",
+				optional = true,
+				opts = function(_, opts)
+					if (vim.g.colors_name or ""):find("catppuccin") then
+						opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+					end
+				end,
+			},
+		},
 	},
 }
