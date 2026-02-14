7dd61a8a476139e6a8bd21f1f922a5e1d1eb990c
huipengcheng <cheng.huipeng@outlook.com>
2026-02-14 18:22:59 +0800

    hypr/.config/hypr/hypridle.conf

--- hypr/.config/hypr/scripts/load_wallpaper.sh
+++ hypr/.config/hypr/scripts/load_wallpaper.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+set -x
+
 if [ $# -lt 1 ]; then
   exit 1
 fi
 
 WALLPAPER="$1"
-if [ ! -f $WALLPAPER ]; then
+if [[ ! -e $WALLPAPER ]]; then
   exit 1
 fi
 
@@ -13,5 +15,6 @@ if ! pgrep hyprpaper >/dev/null; then
   hyprctl keyword exec hyprpaper
 fi
 
-hyprctl hyprpaper reload ,"$WALLPAPER"
-hyprctl hyprpaper unload unused
+# hyprctl hyprpaper reload ,"$WALLPAPER"
+# hyprctl hyprpaper unload unused
+hyprctl hyprpaper wallpaper ", $WALLPAPER, "
