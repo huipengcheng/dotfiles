fdf4816ea0373fc3113e4ae0275aece5eaafaf83
huipengcheng <cheng.huipeng@outlook.com>
2026-02-19 19:54:43 +0800

    nvim: add default value for XDG_CONFIG_HOME

--- nvim/post_install.sh
+++ nvim/post_install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-TARGET="$XDG_CONFIG_HOME/nvim"
+TARGET="${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
 
 if [ -L $TARGET ]; then
   rm $TARGET
 fi
 
-ln -sf $XDG_CONFIG_HOME/nvim-kickstart $TARGET
+ln -sf "${XDG_CONFIG_HOME:-$HOME/.config}/nvim-kickstart-2" $TARGET
