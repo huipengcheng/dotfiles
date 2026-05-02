7bb33e28d8b81db6873792c316eeb958c387caab
huipengcheng <cheng.huipeng@outlook.com>
2026-05-02 17:44:47 +0800

    chore(nvim): update treesitter config

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,10 +1,10 @@
+-- tree-sitter-cli install needed
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    -- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    branch = "main",
     lazy = false,
     build = ':TSUpdate',
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = {
         'bash',
